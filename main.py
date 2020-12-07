3144f834b934603bb94ade1239ea8534b8f2a2e9
maksymbudzin <55983798+maksymbudzin@users.noreply.github.com>
2020-12-08 00:25:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,100 +1,76 @@
-from datetime import datetime
-from subprocess import Popen
-from telegram import Bot
-from telegram import Update
-from telegram import InlineKeyboardButton
-from telegram import InlineKeyboardMarkup
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
-from telegram.ext import CallbackQueryHandler
+import sys
+import crypto
+import telebot
 
-from telegrambot.config import TG_TOKEN
+sys.modules['Crypto'] = crypto
 
-#CALLBACK_LEFT = "call_back_left"
-#CALLbACK_RIGHT = "call_back_right"
-#CALLBACK_FOOTER = "call_back_footer"
+token = "1235237928:AAHx2eMyr8aMi2CJSW5h9gubXbA5ObJ-vnI"
 
-#TITLES = {
-  #  CALLBACK_LEFT: "нове повыдомлення",
-   # CALLBACK_FOOTER: "Ще",
-#}
+import pyrebase
 
+config = {
 
-def do_help(bot: Bot, update: Update):
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Перший бот Макса \n"
-             "Тут є доступні команди /help та /time \n"
-             "Також я можу відповісти на любе твоє пов",
+    "apiKey": "AIzaSyBxKG47FDkkL6rUvrQtDRfn-RGh9XbryYA",
 
-    )
+    "authDomain": "pythonfirebasestorage-9eab0.firebaseapp.com",
 
+    "databaseURL": "gs://pythonfirebasestorage-9eab0.appspot.com",
 
-def do_time1(bot: Bot, update: Update):
-    """ Для того щоб дізнатись час на вінді
-    """
-    now = datetime.now()
-    text = "{}.{}.{} {}:{}:{}".format(now.day, now.month, now.year, now.hour, now.minute, now.second)
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Дата та година на вінді: \n{}".format(text),
-    )
+    "projectId": "pythonfirebasestorage-9eab0",
+
+    "storageBucket": "pythonfirebasestorage-9eab0.appspot.com",
+
+    "serviceAccount": "ServiceAccountKey.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+storage = firebase.storage()
+
+path_on_cloud = "Denys(48).csv"
+
+storage.child(path_on_cloud).download(filename="Denys(48).csv", path="Denys(48).csv")
+
+bot = telebot.TeleBot(token)
 
 
-def do_start(bot: Bot, update: Update):
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Привіт, це перший бот Макса \n"
-             "Тут є доступні команди /help та /time \n"
-             "Також я можу відповісти на любе твоє повідомлення",
+@bot.message_handler(content_types=['text'])
+def echo_all(message):
 
-    )
+    with open("C:/Users/admin/PycharmProjects/FirebaseFile/Denys(48).csv", "rb") as file:
+        f = file.read()
 
+    bot.reply_to(message, "Завантажую файл")
 
-def do_echo(bot: Bot, update: Update):
-    text = update.message.text
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=text,
-       # reply_markup=get_base_inline_keyboard()
+    bot.send_document(message.chat.id, open(r'C:/Users/admin/PycharmProjects/FirebaseFile/Denys(48).csv', 'rb'))
 
-    )
+    bot.reply_to(message, "Файл отримано")
 
 
-def main():
-    bot = Bot(
-        token=TG_TOKEN,
-    )
-    updater = Updater(
-        bot=bot,
-    )
+bot.polling()
 
-    start_handler = CommandHandler("start", do_start)
-    help_handler = CommandHandler("help", do_help)
-    time1_handler = CommandHandler("time", do_time1)
+# @bot.message_handler(content_types=['document'])
 
-    message_handler = MessageHandler(Filters.text, do_echo)
+# def handle_docs_file(message):
+#
+#
+#    try:
+#        chat_id = message.chat.id
+#
+#        file_info = bot.get_file(message.document.file_id)
 
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(help_handler)
-    updater.dispatcher.add_handler(time1_handler)
-    updater.dispatcher.add_handler(message_handler)
-    updater.start_polling()
-    updater.idle()
+#        downloaded_file = bot.download_file(file_info.file_path)
+#
+#        src = 'C:/Users/admin/PycharmProjects/FirebaseFile/share_files/Denys(47).csv' + message.document.file_name;
 
-#def get_base_inline_keyboard():
-    #keyboard =[
-      #     InlineKeyboardButton(TITLES[CALLBACK_LEFT]), callbackdata=CALLBACK_LEFT,
-        # ],
-       # [
-            #InlineKeyboardButton(TITLES[CALLBACK_FOOTER]), callbackdata = CALLBACK_FOOTER,
-        #],
-   # ]
-   # return InlineKeyboardButton(KeyError)
+#        with open(src, 'wb') as new_file:
 
+#            new_file.write(downloaded_file)
+#
+#        bot.reply_to(message, "file saved")
 
+#    except Exception as e:
 
-if __name__ == '__main__':
-    main()
+#        bot.reply_to(message, e)
+#
+# bot.polling()
